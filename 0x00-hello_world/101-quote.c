312c67cf2fa8ea332834d4fc8766da9ee4ffeb9f
headply <ogedengbemayo@gmail.com>
2022-07-01 10:26:43 -0700

    i don dey tire

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - to be called
+* return:0 for the int
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
