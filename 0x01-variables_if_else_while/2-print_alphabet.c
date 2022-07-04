6f7c14e82b11aa8a1417f52d9d5b27c74b41a6db
headply <ogedengbemayo@gmail.com>
2022-07-04 13:04:19 -0700

    Betty abeg

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - will print from a to zin small letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a_to_i;
+	for (a_to_z = 'a'; a_to_z <= 'z'; a_to_z++)
+		putchar(a_to_z);
+	putchar('\n');
+	return (0);
+}
