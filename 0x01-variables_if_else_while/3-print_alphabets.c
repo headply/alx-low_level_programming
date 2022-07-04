0ed172e8b43a96765a9743cf739bb9935f39717a
headply <ogedengbemayo@gmail.com>
2022-07-04 14:01:25 -0700

    forsmall and capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print Small and capital a to z
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'a' ; i <= 'z'; i++)
+		putchar(toupper(i));
+	putchar("\n");
+	return (0);
+}
